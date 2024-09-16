83184e1b304f9aaa8d8d37fa8ea9c01f6ed74af8
Kazte <39470734+Kazte@users.noreply.github.com.>
2024-09-16 10:32:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h3 align='center' style='font-size: 3rem;'>Tooltip Enhancement</h1>
+
+---
+
+## Description
+
+TooltipEnhancement is a World of Warcraft addon designed to impove the default tooltip system.
+
+## Features
+
+- Adds a tooltip within the chat frame to display the item link when hovering over an item in the chat.
+
+## Todo
+
+- Adds Outfit support to the tooltip. (show the outfit with a little preview)
---
+++ TooltipEnhancement.lua
@@ -0,0 +1,37 @@
+local function showTooltip(self, linkData)
+  local linkType = string.split(":", linkData)
+
+  if linkType == 'item'
+      or linkType == 'enchant'
+      or linkType == 'spell'
+      or linkType == 'quest'
+      or linkType == 'talent'
+      or linkType == 'glyph'
+      or linkType == 'unit'
+      or linkType == 'achievement' then
+    GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+    GameTooltip:SetHyperlink(linkData)
+    GameTooltip:Show()
+  end
+end
+
+local function hideTooltip(...)
+  GameTooltip:Hide()
+end
+
+local function setOrHookHandler(frame, script, func)
+  if frame:GetScript(script) then
+    frame:HookScript(script, func)
+  else
+    frame:SetScript(script, func)
+  end
+end
+
+
+for i = 1, NUM_CHAT_WINDOWS do
+  local frame = getglobal("ChatFrame" .. i)
+  if frame then -- make sure that the fram exists
+    setOrHookHandler(frame, "OnHyperLinkEnter", showTooltip)
+    setOrHookHandler(frame, "OnHyperLinkLeave", hideTooltip)
+  end
+end
